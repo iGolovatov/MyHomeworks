1ad1507e19a31faa1c9ed5e2477441113e76bb1b
iGolovatov <iGolovatov@gmail.com>
2025-02-02 16:02:26 +0300

    hw_22: Написал функции для работы с CSV файлами

--- hw_22/files_utils.py
+++ hw_22/files_utils.py
@@ -53,3 +53,57 @@ def append_json(*data: dict, file_path: str, encoding: str = 'utf-8') -> None:
     write_json(*new_data, file_path=file_path, encoding=encoding)
 
 
+# Функции для работы с CSV файлами
+
+def read_csv(file_path: str, delimiter=';', encoding: str = 'utf-8') -> list[dict[Any, Any]]:
+    """
+    Функция для чтения csv файла
+    :param file_path: Путь к файлу
+    :param delimiter: Разделитель (по умолчанию ";")
+    :param encoding: Кодировка файла (по умолчанию "utf-8")
+    :return: Данные в виде списка, считанные из файла
+    """
+    try:
+        with open(file_path, encoding=encoding) as f:
+            reader = csv.DictReader(f, delimiter=delimiter)
+            return list(reader)
+    except FileNotFoundError:
+        print(f'Файл {file_path} не найден')
+        return []
+
+
+def write_csv(*data: dict, file_path: str, delimiter=';', encoding: str = 'utf-8') -> None:
+    """
+    Функция для записи данных в csv файл
+    :param data: Данные для записи
+    :param file_path: Путь к файлу
+    :param delimiter: Разделитель (по умолчанию ";")
+    :param encoding: Кодировка файла (по умолчанию "utf-8")
+    :return: None
+    """
+    with (open(file_path, 'w', encoding=encoding) as f):
+        writer = csv.DictWriter(
+            f,
+            delimiter=delimiter,
+            fieldnames=data[0].keys(),
+        )
+        writer.writeheader()
+        writer.writerows(data)
+
+
+def append_csv(*data: dict, file_path: str, delimiter=';', encoding: str = 'utf-8') -> None:
+    """
+    Функция для добавления данных в csv файл
+    :param data: Данные для записи
+    :param file_path: Путь к файлу
+    :param delimiter: Разделитель (по умолчанию ";")
+    :param encoding: Кодировка файла (по умолчанию "utf-8")
+    :return: None
+    """
+    with open(file_path, mode='a', encoding=encoding) as f:
+        csv.DictWriter(
+            f,
+            delimiter=delimiter,
+            fieldnames=data[0].keys(),
+        ).writerows(data)
+
