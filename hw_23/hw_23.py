4b6a789b78d26566b5180ab3be71b12fe87930a1
iGolovatov <iGolovatov@gmail.com>
2025-02-09 17:27:09 +0300

    hw_23: Выполнил второй раздел домашнего задания

--- hw_23/hw_23.py
+++ hw_23/hw_23.py
@@ -39,3 +39,59 @@ def register_user(password: str):
 
 print('Part 1')
 register_user(input('Введите пароль: '))
+
+
+print('Part 2')
+
+def password_validator(
+    min_length: int = 8,
+    min_uppercase: int = 1,
+    min_digits: int = 1,
+    min_lowercase: int = 1,
+    min_special_chars: int = 1,
+):
+    """
+    Функция-декоратор для валидации пароля пользователя
+    :param min_length:
+    :param min_uppercase:
+    :param min_digits:
+    :param min_lowercase:
+    :param min_special_chars:
+    :return:
+    """
+    def decorator(func: Callable[[str, str], None]) -> Callable[[str, str], None]:
+        def wrapper(username: str, password: str) -> None:
+            if len(password) < min_length:
+                raise ValueError(f'Пароль менее {min_length} символов!')
+            if len(re.findall(r'([0-9])', password)) < min_digits:
+                raise ValueError(f'Пароль должен содержать не менее {min_digits} цифр!')
+            if len(re.findall(r'([A-Z])', password)) < min_uppercase:
+                raise ValueError(f'Пароль должен содержать не менее {min_uppercase} заглавных букв!')
+            if len(re.findall(r'([a-z])', password)) < min_lowercase:
+                raise ValueError(f'Пароль должен содержать не менее {min_lowercase} строчных букв!')
+            if len(re.findall(r'[!@#$%^&]', password)) < min_special_chars:
+                raise ValueError(f'Пароль должен содержать не менее {min_special_chars} специальных символов!')
+
+            return func(username, password)
+
+        return wrapper
+
+    return decorator
+
+def username_validator(func: Callable[[str, str], None]) -> Callable[[str, str], None]:
+    """
+    Функция-декоратор для проверки логина пользователя
+    :param func:
+    :return:
+    """
+    def wrapper(username: str, password: str) -> None:
+        if username.find(' ') != -1:
+            raise ValueError('Имя пользователя не должно содержать пробелы')
+
+        return func(username, password)
+
+    return wrapper
+
+file_path = 'Login&Password.csv'
+
+
